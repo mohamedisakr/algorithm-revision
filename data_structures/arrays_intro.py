506ccb79fe7aeda1577df2cf3f9a076417ead2e5
mohamedisakr <mohamedisakr@gmail.com>
2023-07-20 09:14:21 +0300

    arrays intro

---
+++ data_structures/arrays_intro.py
@@ -0,0 +1,41 @@
+from array import array
+
+# my_array = array('i', [1, 2, 3, 4, 5])
+
+# my_array.append(6)
+
+# my_array.insert(0, 10)
+
+# my_extension = array('i', [7, 8, 9, 10])
+# my_array.extend(my_extension)
+
+# c = [11, 12, 13]
+# my_array.fromlist(c)
+
+# my_array.remove(4)
+
+# my_array.pop()
+
+# print(my_array.index(5))
+
+# my_array.reverse()
+
+# print(my_array.buffer_info())
+
+# my_array = array('i', [1, 2, 3, 3, 5])
+# print(my_array.count(3))
+
+# my_char_array = array('c', ['s', 'a', 'k', 'r'])
+# print(my_char_array.tostring())
+
+# my_array = array('i', [1, 2, 3, 4, 5])
+# c = my_array.tolist()
+# print(c)
+
+# my_char_array = array('c', ['g', 'e', 'e', 'k'])
+# my_char_array.fromstring("stuff")
+# print(my_char_array)
+
+
+# for item in my_array:
+#     print(item)
