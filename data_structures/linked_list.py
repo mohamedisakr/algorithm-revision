7e96f53542fba4247ec50b152cc00e576e49a364
mohamedisakr <mohamedisakr@gmail.com>
2023-07-25 16:25:36 +0300

    linked list max min

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,3 +1,5 @@
+# from sys import
+import math  # from math import inf
 from data_structures.node import Node
 
 
@@ -6,6 +8,9 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+        self.max_val = -math.inf
+        self.min_val = math.inf
+        self.total_sum = 0
 
     def __len__(self) -> int:
         return self.size
@@ -23,7 +28,24 @@ class LinkedList:
             temp_head = temp_head.next
         print(f'{None}', end='')
 
+    def max_value(self):
+        return self.max_val
+
+    def min_value(self):
+        return self.min_val
+
+    def sum_total(self):
+        return self.total_sum
+
     def insert(self, value):
+        if value is not None and not isinstance(value, int):
+            # raise an exception if val is not None and not an int
+            raise TypeError("val must be of int data type")
+
+        if value is not None and value < 0:
+            # raise an exception if val is not None and negative
+            raise ValueError("val must be non-negative")
+
         new_node = Node(value)
 
         # empty linked list
@@ -35,3 +57,6 @@ class LinkedList:
             self.tail = new_node
 
         self.size += 1
+        self.max_val = max(self.max_val, value)
+        self.min_val = min(self.min_val, value)
+        self.total_sum += value
--- tests/data_structure/linked_list_test.py
+++ tests/data_structure/linked_list_test.py
@@ -45,10 +45,17 @@ class TestLinkedList(TestCase):
 
     def test_insert_none(self):
         # This method tests the insert method with a None value
-        self.list.insert(None)
-        self.assertIsNone(self.list.tail.val,
-                          "The tail value should be None")
-        self.assertEqual(len(self.list), 4, "The length should be 4")
+        with self.assertRaises(TypeError):
+            # try to create a node with a non-numeric value
+            self.list.insert(None)
+        # with self.assertRaises(ValueError):
+        #     # try to create a node with a negative value
+        #     node = Node(-1)
+
+        # self.list.insert(None)
+        # self.assertIsNone(self.list.tail.val,
+        #                   "The tail value should be None")
+        # self.assertEqual(len(self.list), 4, "The length should be 4")
 
     def test_insert_negative(self):
         # This method tests the insert method with a negative value
@@ -73,9 +80,84 @@ class TestLinkedList(TestCase):
         self.assertEqual(output.getvalue().strip(), expected_output,
                          "The output should match the expected output")
 
+    def test_length(self):
+        self.list.insert(10)
+        self.list.insert(20)
+        self.list.insert(30)
+
+        # check if the size is updated correctly
+        # self.assertEqual(self.list.size, 3)
+        self.assertEqual(len(self.list), 6)
+
+    # def test_insert_1(self):
+    #     # This method tests the insert method of your linked list class
+    #     # You can use self.assertEqual to check if two values are equal
+    #     self.list.insert(10)
+    #     self.list.insert(20)
+    #     self.list.insert(30)
+
+    #     # check if the size is updated correctly
+    #     self.assertEqual(self.list.size, 3)
+
+    #     # check if the head node has the correct value
+    #     self.assertEqual(self.list.head.val, 10)
+
+    #     # check if the tail node has the correct value
+    #     self.assertEqual(self.list.tail.val, 30)
+
+    #     # check if the max value is updated correctly
+    #     self.assertEqual(self.list.max_value(), 30)
+
+    #     # check if the min value is updated correctly
+    #     self.assertEqual(self.list.min_value(), 10)
+
+    #     # check if the sum is updated correctly
+    #     self.assertEqual(self.list.sum_total(), 60)
+
+    # def test_show_node_list_1(self):
+    #     # This method tests the show_node_list method of your linked list class
+    #     # You can use self.assertMultiLineEqual to check if two multi-line strings are equal
+    #     self.list.insert(10)
+    #     self.list.insert(20)
+    #     self.list.insert(30)
+    #     expected_output = "10 -> 20 -> 30 -> None"
+    #     actual_output = self.list.show_node_list()
+    #     # check if the output matches the expected output
+    #     self.assertMultiLineEqual(expected_output, actual_output)
+
+    def test_insert_error(self):
+        # This method tests if the insert method raises an error when the
+        # list is fulist
+        # You can use self.assertRaises to check if an exception is
+        # raised
+        for i in range(10):
+            self.list.insert(i)
+        # check if a ValueError is raised when inserting more than 10
+        # elements
+        # with self.assertRaises(ValueError):
+        #     self.list.insert(10)
+        self.list.insert(10)
+        self.assertEqual(len(self.list), 14)
+
 
 if __name__ == "__main__":
     # This block runs the test suite when the script is executed
     suite = TestSuite()
     suite.addTest(makeSuite(TestLinkedList))
     TextTestRunner().run(suite)
+
+# test_linked_list.py
+
+# import unittest
+# from linked_list import LinkedList # assuming your linked list class is in a file named linked_list.py
+
+# class TestLinkedList(unittest.TestCase):
+
+#     def setUp(self):
+#         # This method is executed before each test method
+#         # You can use it to create a test instance of your linked list class
+#         self.ll = LinkedList()
+
+
+# if __name__ == "__main__":
+#     unittest.main() # run all the tests in this file
